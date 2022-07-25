c08f3d7dcc88e263acd3c823e909602060e479fb
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2022-07-25 00:44:40 -0500

    creating the models

---
+++ models/Thought.js
@@ -0,0 +1,50 @@
+const { Schema, model } = require('mongoose');
+
+const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        maxLength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [
+        {
+            type: Schema.Types.ObjectId,    
+            ref: 'Reaction'
+        } 
+    ] 
+});
+
+const ReactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    }
+});
+
+// get the length of the thought's reactions array
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
---
+++ models/User.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+        // add a validator to the email field
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ] 
+});
+
+// get the length of the user's friends array
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+// create the User model using the UserSchema
+const User = model('User', UserSchema);
+
+// export the User model
+module.exports = User;
